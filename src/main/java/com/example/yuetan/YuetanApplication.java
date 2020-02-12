b25a48dd0c48ef892604180a23f82bfe05395716
sunruiyi <630258678@qq.com>
2020-02-12 17:45:07 +0800

    创建项目

---
+++ src/main/java/com/example/yuetan/YuetanApplication.java
@@ -0,0 +1,13 @@
+package com.example.yuetan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuetanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuetanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yuetan/YuetanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yuetan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuetanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
