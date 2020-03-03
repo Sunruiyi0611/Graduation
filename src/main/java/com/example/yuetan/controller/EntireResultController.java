37aaf3de815940fa182c3cbc67944f5535ab6f44
sunruiyi <630258678@qq.com>
2020-03-03 22:41:35 +0800

    展示简单的折线图

--- src/main/java/com/example/yuetan/controller/EntireResultController.java
+++ src/main/java/com/example/yuetan/controller/EntireResultController.java
@@ -1,31 +1,47 @@
 package com.example.yuetan.controller;
 
-import com.example.yuetan.dao.EntireResultDao;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.yuetan.model.EntireResult;
 import com.example.yuetan.service.EntireResultService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class EntireResultController {
+
+    private static final Logger logger = LoggerFactory.getLogger(EntireResultController.class);
 
     @Autowired
     private EntireResultService entireResultService;
 
     @ResponseBody
     @RequestMapping("/getEntireResult")
-    public String getEntireResult(@RequestParam String loc,
-                                  @RequestParam String dir,
-                                  @RequestParam String tim){
+    public Map<String,Object> getEntireResult(Model model,
+                               @RequestParam String loc,
+                               @RequestParam String dir,
+                               @RequestParam String tim){
 
         List<EntireResult> results =entireResultService.getEntireResultByLDT(loc,dir,tim);
 
-        return results.toString();
+        logger.info("return results={}",results.toString());
+
+        JSONArray array= JSONArray.parseArray(JSONObject.toJSONString(results));
+
+        Map<String,Object> resultMap =new HashMap<String, Object>();
+        resultMap.put("res",array);
+
+        return resultMap;
     }
 
     @ResponseBody
--- src/main/java/com/example/yuetan/service/impl/EntireResultServiceImpl.java
+++ src/main/java/com/example/yuetan/service/impl/EntireResultServiceImpl.java
@@ -6,6 +6,8 @@ import com.example.yuetan.service.EntireResultService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -16,6 +18,13 @@ public class EntireResultServiceImpl implements EntireResultService {
 
     @Override
     public List<EntireResult> getEntireResultByLDT(String location, String direction, String time) {
-        return entireResultDao.getEntireResultByLocDirTim(location,direction,time);
+        List<EntireResult> list =entireResultDao.getEntireResultByLocDirTim(location,direction,time);
+        Collections.sort(list, new Comparator<EntireResult>() {
+            @Override
+            public int compare(EntireResult o1, EntireResult o2) {
+                return (int) (o1.getSpecific_time()-o2.getSpecific_time());
+            }
+        });
+        return list;
     }
 }
